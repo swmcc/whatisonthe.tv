daf40f7349f6ea53e7232d93b26bcf8043154c38
Stevie McCullough <stephen.mccullough@gmail.com>
2023-09-25 12:54:20 +0100

    ✨ - Amended search page layout and added in icons for type.

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,18 @@
 import { useState } from 'react';
+import { FilmIcon, UserIcon, TvIcon } from '@heroicons/react/20/solid'
+
+const ICONS = {
+  movie: <FilmIcon aria-label='Movie' className="inline-block h-4 w-4" />,
+  person: <UserIcon aria-label='Person' className="inline-block h-4 w-4" />,
+  series: <TvIcon aria-label='TV' className="inline-block h-4 w-4" />
+};
 
 const Search = () => {
   const [query, setQuery] = useState('');
   const [data, setData] = useState(null);
 
   const fetchData = async () => {
-    if (query.length >= 3) {
+    if (query.length >= 5) {
       const DOMAIN = process.env.REACT_APP_WHATISONTHETV_API_DOMAIN
       const response = await fetch(`${DOMAIN}/shows?query=${query}`);
       const jsonData = await response.json();
@@ -38,19 +45,26 @@ const Search = () => {
         {data ? data.map((item) => (
           <li key={item.id} className="relative">
 
-            <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-              <img src={item.thumbnail} alt={item.name} className="pointer-events-none object-cover group-hover:opacity-75" />
-              <button type="button" className="absolute inset-0 focus:outline-none">
-                <span className="sr-only">View details for {item.name}</span>
-              </button>
+            <div className="w-[300px] h-[450px] group block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 flex justify-center items-center">
+              {item.thumbnail ? (
+                <img src={item.thumbnail} alt={item.name} className="object-contain h-full w-full" />
+              ) : (
+                <div className="w-[300px] h-[450px] bg-gray-200 flex items-center justify-center">
+                  <span>No Image Available</span>
+                </div>
+              )}
             </div>
-            <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{item.name}</p>
-            <p className="pointer-events-none block text-sm font-medium text-gray-500">{item.year}</p>
-            <p className="pointer-events-none block text-sm font-medium text-gray-500">{item.type}</p>
+            <p className="text-base text-left block">
+              <span className="text-sm font-medium text-gray-900">{item.name}</span>
+            </p>
+            <p className="text-sm text-left block">
+              <span className="text-sm font-medium text-gray-500">{item.year}{' '}</span>
+              {ICONS[item.type] || ""}
+            </p>
           </li>
         )) : null}
       </ul>
-    </div>
+    </div >
   );
 };
 
